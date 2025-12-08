556effdfb684422be9803f89dbae3ef166a41615
Sean-McLean37 <smclean2@umd.edu>
2025-12-08 01:46:35 -0500

    Update proj3_CSV_data_source.py

--- src/proj3_CSV_data_source.py
+++ src/proj3_CSV_data_source.py
@@ -1,3 +1,14 @@
+"""
+Crime Research Data Pipeline - Class Definition For CSV Data
+
+This module defines CSVDataSource, an abstract derived class.
+
+Author: INST326 Crime Research Data Pipeline Project Team (Group 0203-SAV-ASMV)
+Course: Object-Oriented Programming for Information Science
+Institution: University of Maryland, College Park
+Project: Advanced OOP with Inheritance & Polymorphism (Project 3)
+"""
+
 #ABSTRACT Derived Class
 from pathlib import Path
 import pandas as pd
