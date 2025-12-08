5b98a3a982f62b04918ddf0449f67a3d2631ed67
Sean-McLean37 <smclean2@umd.edu>
2025-12-08 00:30:46 -0500

    Update proj3_demonstration.py

--- examples_and_tests/proj3_demonstration.py
+++ examples_and_tests/proj3_demonstration.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
+"""
+Crime Research Data Pipeline - Core Classes Demo Script
 
+This script demonstrates the Project 3 implementation of the crime
+research data pipeline project satisfying all Project 3 requirements:
+- Inheritance hierarchies
+- Polymorphic behavior
+- Abstract base classes
+- Composition relationships
+
+Author: INST326 Crime Research Data Pipeline Project Team (Group 0203-SAV-ASMV)
+Course: Object-Oriented Programming for Information Science
+Institution: University of Maryland, College Park
+Project: Advanced OOP with Inheritance & Polymorphism (Project 3)
+"""
