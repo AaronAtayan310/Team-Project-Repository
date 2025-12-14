67626707f548a1fa29954f0bfc9177872e56a50f
AaronAtayan310 <aatayan@terpmail.umd.edu>
2025-12-13 22:49:05 -0500

    Update proj4_testing_suite.py

--- examples_and_tests/proj4_testing_suite.py
+++ examples_and_tests/proj4_testing_suite.py
@@ -22,7 +22,7 @@ import os
 from datetime import datetime
 
 # Add src directory to path so we can import the relevant code files
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'src'))
+sys.path.insert(0, str(Path(__file__).parent.parent / 'src'))
 
 from proj4_data_source import DataSource
 from proj4_data_processor import DataProcessor
